bdc15a5dc65f9857d9d983038774a8aaecb26d56
PapageiSquirrel <raishinshi@hotmail.com>
2016-11-10 22:10:23 +0100

    remove $$hashkey from array

--- app/services.js
+++ app/services.js
@@ -41,13 +41,7 @@ common.factory('dataFactory', ['$http', function($http) {
 		$http({
 			url: '/data/find=' + col,
 			method: 'POST',
-			data: JSON.stringify(data, function( key, value ) {
-				if( key === "$$hashKey" ) {
-					return undefined;
-				}
-
-				return value;
-			}),
+			data: angular.toJson(data),
 			headers: {'Content-Type': 'application/json'}
 		}).success(function (data, status, headers, config) {
 			//$scope.msg = 'Le serveur a chargé la collection ' + col + ' !\n';
